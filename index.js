cb55082da057fdbbd29ecd0ff511c90d110f89fa
sr294 <81427942+sr294@users.noreply.github.com>
2023-05-03 17:27:37 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.tab').on('click',function() {
+    $('.tab, .panel').removeClass('active');
+    $(this).addClass('active');
+
+    var index = $('.tab').index(this);
+    $('.panel').eq(index).addClass('active');
+  });
+});
