f84a9f45f42b0e93df01910f69f91507bf5e195b
Tomei-n <81427942+Tomei-n@users.noreply.github.com>
2023-07-06 16:26:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ $(function() {
     $('.panel').eq(index).addClass('active');
   });
 });
-
 $(window).on('load',function() {
   $('.loading').hide();
 });
+
+
+
+
+$(".deliv").click(function() {
+  $('.modal-wrapper').css({'overscroll-behavior-y':'contain'});
+  $('.deliv-modal').fadeIn();
+});
+$('.close-modal , .overlay').click(function(){
+  $('.deliv-modal').fadeOut();
+});
+
